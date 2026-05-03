6f0d3673622ef87177d43f9bb568d991d10ec9f0
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2026-05-03 20:42:32 +0530

    feat: add MockitoBean for JavaMailSender in NotificationServiceApplicationTests

--- services/notification-service/src/test/java/com/rajitha/ecommerce/NotificationServiceApplicationTests.java
+++ services/notification-service/src/test/java/com/rajitha/ecommerce/NotificationServiceApplicationTests.java
@@ -2,10 +2,13 @@ package com.rajitha.ecommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class NotificationServiceApplicationTests {
-
+	@MockitoBean
+	private JavaMailSender javaMailSender;
 	@Test
 	void contextLoads() {
 	}
