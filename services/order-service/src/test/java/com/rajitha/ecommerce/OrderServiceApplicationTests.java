48583148c25caed99688de4f2b6684b3ad8aab31
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2026-05-03 20:23:29 +0530

    feat: add distribution management to api-gateway and enhance order service tests with Mockito

--- services/order-service/src/test/java/com/rajitha/ecommerce/OrderServiceApplicationTests.java
+++ services/order-service/src/test/java/com/rajitha/ecommerce/OrderServiceApplicationTests.java
@@ -1,10 +1,29 @@
 package com.rajitha.ecommerce;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest(properties = {
+		"spring.cloud.config.enabled=false",
+		"spring.config.import=",
+		"application.config.customer-url=http://localhost:8090",
+		"application.config.payment-url=http://localhost:8091",
+		"application.config.product-url=http://localhost:8092"
+})
+@ExtendWith(MockitoExtension.class)
 class OrderServiceApplicationTests {
+	@MockitoBean
+	private KafkaTemplate<String, Object> kafkaTemplate;
+
+	@MockitoBean
+	private KafkaAdmin kafkaAdmin;
 
 	@Test
 	void contextLoads() {
