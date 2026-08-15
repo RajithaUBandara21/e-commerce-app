0827871e7c7156f73285ef3da9ada6a56625e856
Rajitha Bandara <138967884+RajithaUBandara21@users.noreply.github.com>
2026-08-15 16:09:17 +0530

    feat(security): allow public access to product API endpoints

--- services/api-gateway/src/main/java/com/rajitha/apigateway/config/SecurityConfiguration.java
+++ services/api-gateway/src/main/java/com/rajitha/apigateway/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -37,6 +38,8 @@ public class SecurityConfiguration {
                 .authorizeExchange(exchange ->exchange
                         .pathMatchers("/eureka/**")
                 .permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/v1/products/**")
+                        .permitAll()
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 ->oauth2.jwt(Customizer.withDefaults()));
